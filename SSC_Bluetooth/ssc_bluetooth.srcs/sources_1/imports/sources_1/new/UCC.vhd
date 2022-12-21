53830898893effa15ec5c776bef82094edabd3b3
Bogdan <pupezab@yahoo.com>
2022-12-21 16:47:47 +0200

    Revert "update"
    This reverts commit 0c8a05f76a484961d9198c519f220a3a5356bdea.

--- SSC_Bluetooth/ssc_bluetooth.srcs/sources_1/imports/sources_1/new/UCC.vhd
+++ SSC_Bluetooth/ssc_bluetooth.srcs/sources_1/imports/sources_1/new/UCC.vhd
@@ -84,14 +84,14 @@ when incarcare_date=>stare_urm<=octet1;
                      --oc1<=date_intrare(15 downto 8);
                      --oc2<=date_intrare(7 downto 0);
 when octet1=>stare_urm<=transmisie1;
-             date_iesire<=date_intrare(15 downto 8);
+             date_iesire<=oc1;
 when transmisie1=>if activ='0' and done='1' then
                         stare_urm<=octet2;
                   else
                         stare_urm<=transmisie1;
                   end if;
 when octet2=>stare_urm<=transmisie2;
-             date_iesire<=date_intrare(7 downto 0);
+             date_iesire<=oc2;
 when transmisie2=>if activ='0' and done='1' then
                         stare_urm<=stop;
                   else
