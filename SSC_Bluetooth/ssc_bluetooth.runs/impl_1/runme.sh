c42c82bc75bdee24684c532d7e2b3d2a90b433f6
Bogdan <pupezab@yahoo.com>
2023-01-17 23:43:08 +0200

    last commit

--- SSC_Bluetooth/ssc_bluetooth.runs/impl_1/runme.sh
+++ SSC_Bluetooth/ssc_bluetooth.runs/impl_1/runme.sh
@@ -41,7 +41,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log Nexys4.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source Nexys4.tcl -notrace
 
 
